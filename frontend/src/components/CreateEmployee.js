5422853ce6519fa7c24419a80d3df326190d80b4
fabianko <fabian.arismendi@usach.cl>
2020-10-19 22:58:25 -0300

    ADD: axios para peticiones rest

--- frontend/src/components/CreateEmployee.js
+++ frontend/src/components/CreateEmployee.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreateEmployee extends Component {
+    state = {
+
+    }
+
+
     render() {
         return (
             <div className="card"> 
                 <div className="card-body">
                     <h1>Formulario</h1>
                 </div>
-          
             </div>
         )
     }

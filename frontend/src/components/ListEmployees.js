6f27b3f7a31e80f73590329432948a1d682b50cd
fabianko <fabian.arismendi@usach.cl>
2020-10-20 21:08:06 -0300

    ADD: se agrega servicio rest para traer listado de empleados

--- frontend/src/components/ListEmployees.js
+++ frontend/src/components/ListEmployees.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class ListEmployees extends Component {
+    URL_API_EMPLOYEES = 'http://localhost:4000/api/employees';
+    state = {
+        employees: []
+    }
+
+    async componentDidMount() {
+        this.getEmployees();
+    }
+
+    getEmployees = async () => {
+        const res = await axios.get(this.URL_API_EMPLOYEES);
+        console.log(res.data)
+        this.setState({ employees: res.data});
+    }
+
     render() {
         return (
             <div> Listar empleados</div>
-        )
+            )
     }
 }

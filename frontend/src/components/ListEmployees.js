3ead6c863d6c2b7add7221bd9591728623ff61d3
fabianko <fabian.arismendi@usach.cl>
2020-11-02 13:03:37 -0300

    ADD: se agrega la opcion de listar empleados

--- frontend/src/components/ListEmployees.js
+++ frontend/src/components/ListEmployees.js
@@ -1,25 +1,48 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class ListEmployees extends Component {
-    URL_API_EMPLOYEES = 'http://localhost:4000/api/employees';
-    state = {
-        employees: []
-    }
+  URL_API_EMPLOYEES = "http://localhost:4000/api/employees";
+  state = {
+    employees: [],
+  };
 
-    async componentDidMount() {
-        this.getEmployees();
-    }
+  async componentDidMount() {
+    this.getEmployees();
+  }
 
-    getEmployees = async () => {
-        const res = await axios.get(this.URL_API_EMPLOYEES);
-        console.log(res.data)
-        this.setState({ employees: res.data});
-    }
+  getEmployees = async () => {
+    const res = await axios.get(this.URL_API_EMPLOYEES);
+    console.log(res.data);
+    this.setState({ employees: res.data });
+  };
 
-    render() {
-        return (
-            <div> Listar empleados</div>
-            )
-    }
+  render() {
+    return (
+        <div>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>office</th>
+                <th>position</th>
+                <th>salary</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.employees.map((user, index) => {
+                return (
+                  <tr key={user._id}>
+                    <td>{user.name}</td>
+                    <td>{user.office}</td>
+                    <td>{user.position}</td>
+                    <td>{user.salary}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+    );
+  }
 }
